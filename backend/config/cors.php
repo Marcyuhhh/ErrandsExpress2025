65265e775c441d232719a08b9e2c04903c5248e7
Marc Eurese <eurese.marc.ariel@gmail.com>
2025-12-19 05:32:59 +0800

    commit

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -7,6 +7,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
+        env('FRONTEND_URL'),
         'http://localhost:5173', // Vite default
         'http://localhost:3000', // React default
         'http://127.0.0.1:5173',
