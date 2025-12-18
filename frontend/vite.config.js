621dd554c7b60f2310381704aee19f5d6af2ce38
Marc Eurese <eurese.marc.ariel@gmail.com>
2025-12-19 05:57:59 +0800

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'frontend-production-270d.up.railway.app', // Add your specific Railway URL here
+    ],
+  },
 })
