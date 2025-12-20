05f162bff39fe50182139ceb5fe7cd589ab3761c
Marc Eurese <eurese.marc.ariel@gmail.com>
2025-12-20 23:17:57 +0800

    adding email

--- backend/resources/views/emails/user_verified.blade.php
+++ backend/resources/views/emails/user_verified.blade.php
@@ -4,6 +4,6 @@
     <h1>Welcome, {{ $user->firstname }}!</h1>
     <p>Your account has been successfully verified by our admins.</p>
     <p>You can now log in to the application and start posting or running errands.</p>
-    <p>Login here: <a href="YOUR_FRONTEND_URL/login">Login</a></p>
+    <p>Login here: <a href="https://errandsexpress.up.railway.app/auth">Login</a></p>
 </body>
 </html>
